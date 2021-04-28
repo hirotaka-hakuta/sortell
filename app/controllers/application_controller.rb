a71516e5771121ee9759082592989f686e518663
hirotaka-hakuta <hakuta.hirotaka@gmail.com>
2021-04-28 11:44:16 +0000

    DMM投稿版

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  # rescue_from Exception, with: :render_500
+  rescue_from Exception, with: :render_500
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     posts_path
   end
 
